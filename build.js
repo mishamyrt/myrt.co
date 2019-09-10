3b033b4fe1d9f691eb11311b1499e4d2348999fd
mishamyrt <misha@myrt.co>
2019-09-10 13:37:05 +0300

    Added cleanup

--- build.js
+++ build.js
@@ -1,18 +1,24 @@
 const { join } = require('path')
 const Bundler = require('parcel-bundler')
+const rimram = require('rimraf')
 
-let serve = process.argv.length > 2 && process.argv[2] === '--serve'
+const serve = process.argv.length > 2 && process.argv[2] === '--serve'
+
+const outDir = join(__dirname, 'dist')
 
 const bundler = new Bundler(join(__dirname, 'src', 'index.pug'), {
     sourceMaps: serve,
     watch: !serve,
-    hmr: serve
+    hmr: serve,
+    outDir
 })
 
 async function build () {
-  const action = serve ? bundler.serve() : bundler.bundle()
-  await action
-  if (!serve) process.exit(0)
+  rimram(outDir, async () => {
+    const action = serve ? bundler.serve() : bundler.bundle()
+    await action
+    if (!serve) process.exit(0)
+  })
 }
 
 build()
