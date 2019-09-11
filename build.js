afe7deeb47427726e41405fcdede37c3246459bd
mishamyrt <misha@myrt.co>
2019-09-11 22:45:17 +0300

    Optimised build

--- build.js
+++ build.js
@@ -9,6 +9,7 @@ const rimram = promisify(require('rimraf'))
 const readDir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
+const unlink = promisify(fs.unlink)
 
 const tp = new Typograf({
   locale: ['ru', 'en-US'],
@@ -18,20 +19,15 @@ const tp = new Typograf({
   },
 })
 
-const serve = process.argv.length > 2 && process.argv[2] === '--serve'
-
 const outDir = join(__dirname, 'dist')
+const indexPath = join(outDir, 'index.html')
 
 const bundler = new Bundler(join(__dirname, 'src', 'index.pug'), {
-    sourceMaps: serve,
-    watch: serve,
-    hmr: serve,
+    sourceMaps: false,
+    watch: false,
+    hmr: false,
     outDir
 })
-
-function build () {
-  return (serve ? bundler.serve() : bundler.bundle())
-}
 
 function improveTypography (filePath) {
   return readFile(filePath)
@@ -47,18 +43,21 @@ function improveTypography (filePath) {
     .then((html) => writeFile(filePath, html))
 }
 
-bundler.on('bundled', (bundle) => {
+bundler.on('bundled', () => {
   let files
-  const indexPath = join(outDir, 'index.html')
-  readDir(join(outDir, 'ru'))
+  readFile(indexPath)
+    .then((html) => writeFile(join(outDir, 'ru', 'index.html'), html))
+    .then(() => unlink(indexPath))
+    .then(() => readDir(join(outDir, 'ru')))
     .then((result) => (files = result))
-    .then(() => files.forEach((fileName) => fileName.includes('.html') ?
-      improveTypography(join(outDir, 'ru', fileName)) : null))
-  !serve && readFile(indexPath)
-    .then((html) => writeFile(indexPath, html.toString().replace(/index.html/g, '')))
+    .then(() => files.forEach((fileName) => {
+      if (fileName.includes('.html')) {
+        improveTypography(join(outDir, 'ru', fileName))
+      }
+    }))
 })
 
 rimram(outDir)
-  .then(() => build())
-  .then(() => !serve ? process.exit(0) : null)
+  .then(() => bundler.bundle())
+  .then(() => process.exit(0))
 
