3bc4ec9ef68b70148391e417a04a2e750763f8fe
mishamyrt <misha@myrt.co>
2020-01-05 00:59:59 +0300

    WIP: Added new templates and styles

---
+++ src/pug.config.js
@@ -0,0 +1,22 @@
+const keyToUrl = key => `/pages/${key}/${key}.pug`
+
+module.exports = {
+  locals: {
+    menuContext: {
+      isActive: (key, url) => url.includes(key),
+      isChild: url => url.split('/').length > 2,
+      items: [
+        { title: 'Blog', key: 'blog' },
+        { title: 'Resume', key: 'resume' }
+      ].map(item => {
+        item.url = keyToUrl(item.key)
+        return item
+      })
+    }
+  }
+}
+
+// ('blog/article', 'blog') => true, true
+// ('blog/', 'blog') => true, false
+// ('blog/', 'resume') => false, false
+// ('blog/', 'resume') => false, false
