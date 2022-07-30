5ec9edbf8504f94bfd1601a4f64918b3c9a923fb
mishamyrt <misha@myrt.co>
2022-07-30 19:10:06 +0300

    Rename lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import typograf from 'astro-typograf-integration'
+import typograf from 'astro-typograf'
 
 export default defineConfig({
   site: 'https://myrt.co',
